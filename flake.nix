6277fca7639dcbb6e63a691b644df60417dd3a7c
kghugo <hugosum.dev@protonmail.com>
2021-10-17 14:40:46 +0100

    Complete derivation for server setup

--- flake.nix
+++ flake.nix
@@ -19,11 +19,42 @@
           ({ pkgs, ... }: {
             nixpkgs.config = { allowUnfree = true; };
 
+            nix = {
+              package = pkgs.nixUnstable;
+              extraOptions = ''
+                experimental-features = nix-command flakes
+              '';
+              binaryCaches = [ "https://cache.nixos.org" ];
+              trustedBinaryCaches =
+                [ "http://cache.nixos.org" "http://hydra.nixos.org" ];
+              optimise = {
+                automatic = true;
+                dates = [ "12:00" ];
+              };
+              gc = {
+                automatic = true;
+                dates = "daily";
+                options = "--delete-older-than 10d";
+              };
+            };
+
+            security.sudo = {
+              enable = true;
+              wheelNeedsPassword = false;
+            };
+
             # Essential
-            environment.systemPackages = with pkgs; [ neovim git ];
+            environment.systemPackages = with pkgs; [
+              neovim
+              git
+              arion
+              docker-client
+            ];
 
             services.nginx = {
               enable = true;
+              #NOTE So that the configuration file can be found in /etc/nginx/nginx.conf for debug
+              enableReload = true;
               recommendedGzipSettings = true;
               recommendedTlsSettings = true;
               recommendedOptimisation = true;
@@ -31,16 +62,54 @@
               virtualHosts."bot" = {
                 addSSL = true;
                 enableACME = true;
+                locations."/" = { proxyPass = "http://localhost:3000"; };
+              };
+            };
+
+            virtualisation.docker.enable = false;
+            virtualisation.podman = {
+              enable = true;
+              dockerSocket.enable = true;
+              defaultNetwork.dnsname.enable = true;
+            };
+
+            users = {
+              mutableUsers = false;
+              users = {
+                hugosum = {
+                  name = "hugosum";
+                  isNormalUser = true;
+                  home = "/home/hugosum";
+                  extraGroups = [
+                    "wheel"
+                    "networkmanager"
+                    "docker"
+                    "input"
+                    "video"
+                    "audio"
+                    "sound"
+                    "podman"
+                  ];
+                  hashedPassword =
+                    "$6$pHSJA2UTMz$Z5IS7T6E67bshhmPfcAQRRKgbEuOelR23SiB5Os0YqUqX.oDl5P/nhnKbSAYmiU1mHn01tJ90HD11dYQpg1iN0";
+                };
               };
             };
 
             networking.useDHCP = false;
             networking.firewall.allowedTCPPorts = [ 80 443 ];
 
+            services.openssh = {
+              enable = true;
+              banner = "";
+            };
+
             security.acme = {
               acceptTerms = true;
-              email = "REDACTED";
+              email = "hugosum.dev@protonmail.com";
             };
+
+            system.autoUpgrade.enable = false;
           })
         ];
       };
