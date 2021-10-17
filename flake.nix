64a94820c3c261b008f66e6db87055ba408a774d
kghugo <hugosum.dev@protonmail.com>
2021-10-17 17:44:44 +0100

    Add HM module to remove existing HM files in current system

--- flake.nix
+++ flake.nix
@@ -3,19 +3,30 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
     hardware = {
       flake = false;
       url = "path:/etc/nixos/hardware-configuration.nix";
     };
   };
 
-  outputs = { self, nixpkgs, hardware, ... }:
+  outputs = { self, nixpkgs, hardware, home-manager, ... }:
     let system = "x86_64-linux";
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
           (import hardware)
+          #NOTE Load home-manager options, so that the default HM modules on that system can be removed
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hugosum = {};
+          }
           ({ pkgs, ... }: {
             nixpkgs.config = { allowUnfree = true; };
 
@@ -49,6 +60,12 @@
               git
               arion
               docker-client
+              # For better listing
+              lsd
+              # For monitoring and debugging
+              bottom
+              ripgrep
+              fd
             ];
 
             services.nginx = {
